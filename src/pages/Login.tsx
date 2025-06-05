6d818ee108957e3922103e47675f17b6650bb05e
Milano <milan@wintruff.de>
2025-06-05 15:04:15 +0200

    fixed weird compiler issues

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/HeaderLogin';
 import Footer from '@/components/Footer';
-import LoginForm from '@/components/LoginForm';
+import LoginForm from "@/components/LoginForm"
 
 const Login = () => {
     return(
