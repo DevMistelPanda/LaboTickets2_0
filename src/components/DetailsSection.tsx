9c5947d0254f5fc75b095392d586ce59223357dc
Milano <milan@wintruff.de>
2025-06-05 12:04:47 +0200

    updated Map Previews

--- src/components/DetailsSection.tsx
+++ src/components/DetailsSection.tsx
@@ -56,7 +56,7 @@ const DetailsSection = () => {
 
           <div className="rounded-xl overflow-hidden shadow-md h-[350px]">
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.2219901290355!2d-74.00369674859906!3d40.71329937933185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a23e28c1191%3A0x49f75d3281df052a!2s150%20Park%20Row%2C%20New%20York%2C%20NY%2010007%2C%20USA!5e0!3m2!1sen!2sus!4v1652209323665!5m2!1sen!2sus" 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5186.574405136288!2d11.082098812366588!3d49.460187758048825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57bbc4f14e41%3A0xf3b84176aa35f091!2sLabenwolf-Gymnasium%20N%C3%BCrnberg!5e0!3m2!1sde!2sde!4v1749117811019!5m2!1sde!2sde" 
               width="100%" 
               height="100%" 
               style={{ border: 0 }}
